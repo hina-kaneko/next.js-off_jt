524e6f3121c591ee61f859ac1e2ae6d7203da49b
hina-kaneko <hina.kaneko@metateam.co.jp>
2025-06-06 11:00:43 +0900

    javaSclipt課題1&2

---
+++ public/nameInput.js
@@ -0,0 +1,10 @@
+document.getElementById("complete").addEventListener("click", function () {
+  const name = document.getElementById("nameInput").value.trim();
+  const message = document.getElementById("message");
+
+  if (value === "") {
+    message.innerText = "名前を入力してください";
+  } else {
+    message.innerText = `こんにちは、${value}さん！`;
+  }
+});
